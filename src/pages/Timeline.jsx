bbc6038bcd25de4249d570ac8778d6398bad5e01
nhrakib777-coder <rakibnh93@gmail.com>
2026-04-16 19:55:32 +0800

    optinal section added sort

--- src/pages/Timeline.jsx
+++ src/pages/Timeline.jsx
@@ -6,7 +6,9 @@ import { FaVideo } from "react-icons/fa";
 function Timeline() {
 
   const { interactions } = useContext(TimelineContext);
+
   const [filter, setFilter] = useState("All");
+  const [sortOrder, setSortOrder] = useState("All");
 
   const getIcon = (type) => {
     if (type === "Call") return <MdCall className="text-xl" />;
@@ -19,33 +21,64 @@ function Timeline() {
       ? interactions
       : interactions.filter((item) => item.type === filter);
 
+  let sortedTimeline = [...filteredTimeline];
+
+  if (sortOrder === "newest") {
+    sortedTimeline.sort((a, b) => new Date(b.date) - new Date(a.date));
+  }
+
+  if (sortOrder === "oldest") {
+    sortedTimeline.sort((a, b) => new Date(a.date) - new Date(b.date));
+  }
+
   return (
     <div className="max-w-3xl mx-auto p-6">
 
-      {/* Title */}
-      <h1 className="text-3xl text-[#1f2937] font-bold mb-4">Timeline</h1>
+      {/* Title + Controls */}
+      <div className="flex justify-between items-center mb-6">
 
-      {/* Dropdown Filter */}
-      <select
-        className="select select-bordered text-[#64748b] p-2 mb-6 w-52"
-        value={filter}
-        onChange={(e) => setFilter(e.target.value)}
-      >
-        <option value="All">Filter timeline</option>
-        <option value="Call">Call</option>
-        <option value="Text">Text</option>
-        <option value="Video">Video</option>
-      </select>
+        <h1 className="text-3xl text-[#1f2937] font-bold">
+          Timeline
+        </h1>
+
+        <div className="flex gap-3">
+
+          {/* Filter */}
+          <select
+            className="select  text-[#64748b] w-[180px] p-2"
+            value={filter}
+            onChange={(e) => setFilter(e.target.value)}
+          >
+            <option value="All">All</option>
+            <option value="Call">Call</option>
+            <option value="Text">Text</option>
+            <option value="Video">Video</option>
+          </select>
+
+          {/* Sort */}
+          <select
+            className="select  text-[#64748b] w-[180px] p-2 "
+            value={sortOrder}
+            onChange={(e) => setSortOrder(e.target.value)}
+          >
+            <option value="all">All</option>
+            <option value="newest">Newest</option>
+            <option value="oldest">Oldest</option>
+          </select>
+
+        </div>
+
+      </div>
 
       {/* Timeline List */}
       <div className="space-y-3">
 
-        {[...filteredTimeline].reverse().map((item) => (
+        {sortedTimeline.map((item) => (
 
           <div
             key={item.id}
             className="flex items-center gap-4 p-4 bg-base-100 rounded-lg shadow"
-          > 
+          >
 
             {/* Icon */}
             <div className="text-gray-700">
