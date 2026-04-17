0c3412ad0dbcd059bf00ade643e045c8a9b7561b
nhrakib777-coder <rakibnh93@gmail.com>
2026-04-17 15:20:17 +0800

    not found 404 page added

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,12 +1,26 @@
+import { Link } from "react-router";
+
 function NotFound() {
   return (
-    <div className="text-center py-20">
+    <div className="min-h-screen flex flex-col justify-center items-center text-center px-6">
 
-      <h1 className="text-4xl font-bold">
+      <h1 className="text-7xl font-extrabold text-red-500 mb-4">
         404
       </h1>
 
-      <p>Page Not Found</p>
+      <h2 className="text-2xl font-semibold mb-2">
+        Oops! Page not found
+      </h2>
+
+      <p className="text-gray-500 mb-8 max-w-md">
+        The page you're looking for doesn't exist or may have been moved.
+      </p>
+
+      <Link to="/">
+        <button className="btn bg-[#244D3F] hover:bg-[#1f2937] text-white p-4">
+          Go Back Home
+        </button>
+      </Link>
 
     </div>
   );
